ea6fbe0efe80f35ae90a342427276d54524bebf3
ahmadzaidan99 <56367749+ahmadzaidan99@users.noreply.github.com>
2019-11-20 15:39:26 +0200

    Create Take_a_Ten_Minute_Walk.cpp

---
+++ Take_a_Ten_Minute_Walk.cpp
@@ -0,0 +1,27 @@
+#include<vector>
+
+bool isValidWalk(std::vector<char> walk) {
+  int nwalk = 0;
+  int swalk = 0;
+  int wwalk = 0;
+  int ewalk = 0;
+  
+  if(walk.size()!=10)//if the walk isn't 10 mins just terminate
+    return false;
+  else{
+    for (int i = 0; i < walk.size(); i++){//Count all the steps we are talking
+      if(walk[i]=='n')
+        nwalk++;
+      if(walk[i]=='s')
+        swalk++;
+      if(walk[i]=='w')
+        wwalk++;
+      if(walk[i]=='e')
+        ewalk++;
+    }
+}
+  if(nwalk == swalk && wwalk == ewalk)//determining if we are back to the starting point
+    return true;
+  else
+    return false;
+}
