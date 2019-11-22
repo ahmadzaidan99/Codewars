5235d40b05e68ce8ccd795876e444cc4e8be5dc3
ahmadzaidan99 <56367749+ahmadzaidan99@users.noreply.github.com>
2019-11-22 14:32:14 +0200

    Update Simple_Pig_Translator.py

--- Simple_Pig_Translator.py
+++ Simple_Pig_Translator.py
@@ -1,14 +1,3 @@
 def pig_it(text):
-    sent=text.split()
-    pigged=[]
-    alpha = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ0123456789"
-    for i in range(len(sent)):
-        if sent[i] in alpha and len(sent[i]) == 1:
-            sent[i]+="ay"
-        elif len(sent[i])>1:
-            sent[i] = sent[i][1: -1]+ sent[i][-1]+  sent[i][0]+ "ay"
-
-        pigged.append(sent[i])
-            
-    return " ".join(pigged)
-        
+    pigged = text.split()
+    return ' '.join( [word[1:] + word[:1] + 'ay' if word.isalpha() else word for word in pigged])
