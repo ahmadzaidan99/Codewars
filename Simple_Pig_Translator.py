312a10c1b642bfaf4b6866044b517c10924a719a
ahmadzaidan99 <56367749+ahmadzaidan99@users.noreply.github.com>
2019-11-22 14:28:11 +0200

    Create Simple_Pig_Translator.py

---
+++ Simple_Pig_Translator.py
@@ -0,0 +1,14 @@
+def pig_it(text):
+    sent=text.split()
+    pigged=[]
+    alpha = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ0123456789"
+    for i in range(len(sent)):
+        if sent[i] in alpha and len(sent[i]) == 1:
+            sent+="ay"
+        elif len(sent[i])>1:
+            sent[i] = sent[i][1: -1]+ sent[i][-1]+  sent[i][0]+ "ay"
+
+        pigged.append(sent[i])
+            
+    return " ".join(pigged)
+        
